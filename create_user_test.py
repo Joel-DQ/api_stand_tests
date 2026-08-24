ad78ca55019774e1caf8180a8ecbd37d92d5fc34
JOEL DIAZ QUINTO <personaljdq@outlook.com>
2026-08-24 15:57:52 -0600

    Mi primer commit

---
+++ create_user_test.py
@@ -0,0 +1,96 @@
+import sender_stand_request
+import data
+
+def get_user_body(name):
+    current_body = data.user_body.copy()
+    current_body["firstName"] = name
+    return current_body
+
+# Función de prueba positiva
+def positive_assert(first_name):
+    # El cuerpo de la solicitud actualizada se guarda en la variable user_body
+    user_body = get_user_body(first_name)
+    # El resultado de la solicitud para crear un/a nuevo/a usuario/a se guarda en la variable user_response
+    user_response = sender_stand_request.post_new_user(user_body)
+
+    # Comprueba si el código de estado es 201
+    assert user_response.status_code == 201
+    # Comprueba que el campo authToken está en la respuesta y contiene un valor
+    assert user_response.json()["authToken"] != ""
+
+    # El resultado de la solicitud de recepción de datos de la tabla "user_model" se guarda en la variable "users_table_response"
+    users_table_response = sender_stand_request.get_users_table()
+
+    # String que debe estar en el cuerpo de respuesta
+    str_user = user_body["firstName"] + "," + user_body["phone"] + "," \
+               + user_body["address"] + ",,," + user_response.json()["authToken"]
+
+    # Comprueba si el usuario o usuaria existe y es único/a
+    assert users_table_response.text.count(str_user) == 1
+
+def negative_assert_symbol(first_name):
+    user_body = get_user_body(first_name)
+    user_response = sender_stand_request.post_new_user(user_body)
+
+    assert user_response.status_code == 400
+    assert user_response.json()["code"] == 400
+    assert user_response.json()["message"] == "Has introducido un nombre de usuario no válido. " \
+                                         "El nombre solo puede contener letras del alfabeto latino, "\
+                                         "la longitud debe ser de 2 a 15 caracteres."
+
+def negative_assert_no_firstname(user_body):
+    user_response = sender_stand_request.post_new_user(user_body)
+
+    assert user_response.status_code == 400
+    assert user_response.json()["code"] == 400
+    assert user_response.json()["message"] == "No se han aprobado todos los parámetros requeridos"
+
+
+# Prueba 1. Creación de un nuevo usuario o usuaria
+# El parámetro "firstName" contiene dos caracteres
+# def test_create_user_2_letter_in_first_name_get_success_response():
+#     positive_assert("Aa")
+
+# Prueba 2. Creación de un nuevo usuario o usuaria
+# El parámetro "firstName" contiene 15 caracteres
+# def test_create_user_15_letter_in_first_name_get_success_response():
+#     positive_assert("Aaaaaaaaaaaaaaa")
+
+# Prueba 3. Creación de un nuevo usuario o usuaria
+# El parámetro "firstName" contiene 1 caracter
+# def test_create_user_1_letter_in_first_name_get_error_response():
+#     negative_assert_symbol("A")
+
+# Prueba 4. Creación de un nuevo usuario o usuaria
+# El parámetro "firstName" contiene 16 caracter
+# def test_create_user_16_letter_in_first_name_get_error_response():
+#     negative_assert_symbol("Aaaaaaaaaaaaaaaa")
+
+# Prueba 5. No se permiten espacios
+# def test_create_user_has_space_in_first_name_get_error_response():
+#     negative_assert_symbol("A Aaa")
+
+# Prueba 6. No se permiten caracteres especiales
+# def test_create_user_has_special_symbol_in_first_name_get_error_response():
+#     negative_assert_symbol("\"№%@\",")
+
+# Prueba 7. No se permiten numeros
+# def test_create_user_has_number_in_first_name_get_error_response():
+#     negative_assert_symbol("123")
+
+# Prueba 8. el parametro no se pasa
+# def test_create_user_no_first_name_get_error_response():
+#      user_body = data.user_body.copy()
+#      user_body.pop("firstName")
+#      negative_assert_no_firstname(user_body)
+
+# Prueba 9. el parametro se pasa vacio
+# def test_create_user_empty_first_name_get_error_response():
+#     user_body = get_user_body("")
+#     negative_assert_no_firstname(user_body)
+
+# Prueba 10. Número
+# def test_create_user_number_type_first_name_get_error_response():
+#      user_body = get_user_body(12)
+#      response = sender_stand_request.post_new_user(user_body)
+#      assert response.status_code == 400
